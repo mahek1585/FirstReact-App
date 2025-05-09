a21f5182533a55060753ce0383803d7a5d17ca6b
unknown <mahek1585@gmail.com>
2025-05-10 00:01:55 +0530

    changes done in ClickCount Application

---
+++ react-program/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ClickProvider } from './Assignment3/ClickContext';
+import Home from './Assignment3/Home';
+// import './index.css';
+
+function App() {
+  return (
+    <ClickProvider>
+      <Home />
+    </ClickProvider>
+  );
+
+}
+
+export default App;
